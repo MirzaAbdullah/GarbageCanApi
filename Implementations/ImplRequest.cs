68fca83e89cfad26a48fb2c5c95a190debff9ae8
Muhammad Abdullah <mirzaabdullah244@gmail.com>
2021-01-25 00:42:56 +0100

    added application services DI's ( IGarbageCan, IRequest ) in Startup.cs

---
+++ Implementations/ImplRequest.cs
@@ -0,0 +1,13 @@
+﻿using GarbageCanApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageCanApi.Implementations
+{
+    public class ImplRequest : IRequest
+    {
+        //Code Here
+    }
+}
