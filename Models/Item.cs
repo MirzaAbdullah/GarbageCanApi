aff0c1f16fdfd753465db0edfca24f77afcbf814
Muhammad Abdullah <mirzaabdullah244@gmail.com>
2021-01-26 15:54:49 +0100

    Database Schema Updated

---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GarbageCanApi.Models
+{
+    public partial class Item
+    {
+        public Item()
+        {
+            RequestDetails = new HashSet<RequestDetail>();
+        }
+
+        public string IdItem { get; set; }
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+
+        public virtual ICollection<RequestDetail> RequestDetails { get; set; }
+    }
+}
---
+++ Models/RequestDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GarbageCanApi.Models
+{
+    public partial class RequestDetail
+    {
+        public string IdRequestDetail { get; set; }
+        public string IdItem { get; set; }
+        public string IdRequest { get; set; }
+        public double ItemWeight { get; set; }
+        public double ItemCost { get; set; }
+
+        public virtual Item IdItemNavigation { get; set; }
+        public virtual Request IdRequestNavigation { get; set; }
+    }
+}
